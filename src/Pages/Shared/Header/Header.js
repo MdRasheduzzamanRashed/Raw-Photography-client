d8ff5bda608dde4c1fbe49827c9da2121609177b
Md Rasheduzzaman Rashed <rased.cse.2016@gmail.com>
2022-11-08 14:39:09 +0600

    header done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../../assets/logo/logo.png";
 
 const Header = () => {
   const menuLists = (
     <>
       <li>
-        <a>Item 1</a>
+        <Link to="/">Home</Link>
       </li>
       <li tabIndex={0}>
-        <a className="justify-between">
-          Parent
+        <Link to="/package" className="justify-between">
+          Package
           <svg
             className="fill-current"
             xmlns="http://www.w3.org/2000/svg"
@@ -19,30 +20,33 @@ const Header = () => {
           >
             <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
           </svg>
-        </a>
+        </Link>
         <ul className="p-2">
           <li>
-            <a>Submenu 1</a>
+            <Link to="/package">Silver Pack</Link>
           </li>
           <li>
-            <a>Submenu 2</a>
+            <Link to="/package">Gold Pack</Link>
+          </li>
+          <li>
+            <Link to="/package">Platinum Pack</Link>
           </li>
         </ul>
       </li>
       <li>
-        <a>Item 3</a>
+        <Link to="/about">About</Link>
       </li>
     </>
   );
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-black">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
+              className="h-6 w-6 text-white"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -62,15 +66,15 @@ const Header = () => {
             {menuLists}
           </ul>
         </div>
-        <Link to="/" className="btn btn-ghost normal-case text-xl">
-          daisyUI
+        <Link to="/">
+          <img src={logo} className="w-12" alt="logo" />
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">{menuLists}</ul>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-primary">Book</button>
+        <button className="btn btn-outline text-white">Book</button>
       </div>
     </div>
   );
